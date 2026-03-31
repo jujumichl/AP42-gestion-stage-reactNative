93fc9fb8337cc50a7cbec9bdc0666c9e2034c5e7
Julien MICHELIN <julienmichelin35@gmail.com>
2026-03-31 17:58:33 +0200

    Ajout du mail dans l'entête de l'application

--- app/navigation/OrganisationsStackNav.js
+++ app/navigation/OrganisationsStackNav.js
@@ -1,19 +1,22 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OrganisationsScreen from "../screens/OrganisationsScreen";
 import OrganisationDetailScreen from "../screens/OrganisationDetailScreen";
+import { useAuth } from '../context/AuthProvider';
 
 const Stack = createNativeStackNavigator();
 // Stack pour la navigation Accueil → Profil
 export default function OrganisationsStack() {
+  const { user } = useAuth();
+
   return (
     <Stack.Navigator
       screenOptions={{
         headerStyle: { backgroundColor: '#3b5bdb' },
         headerTintColor: '#fff',
-        headerTitleStyle: { fontWeight: 'bold' },
+        headerTitleStyle: { fontWeight: 'bold', fontSize: 15 },
       }}
     >
-      <Stack.Screen name="Organisations" component={OrganisationsScreen} />
+      <Stack.Screen name={"Organisations - " + user.email} component={OrganisationsScreen} initialParams={user.email} />
       <Stack.Screen name="OrganisationDetail" component={OrganisationDetailScreen} />
     </Stack.Navigator>
   );
